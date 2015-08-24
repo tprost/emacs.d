2234df30d243ed25bf34acb9a023253cec36e7e7
tprost <tprost@users.noreply.github.com>
2015-08-24 15:34:02 -0400

    Add C-i to toggle god mode

--- init/init-god-mode.el
+++ init/init-god-mode.el
@@ -10,10 +10,10 @@
 
 (global-set-key [f13] 'god-local-mode)
 (global-set-key (kbd "<escape>") 'god-local-mode)
+(global-set-key (kbd "C-i") 'god-local-mode)
 
 (add-to-list 'god-mod-alist '("h" . "s-"))
 
 (define-key god-local-mode-map (kbd "i") 'god-local-mode)
 
 (provide 'init-god-mode)
-
