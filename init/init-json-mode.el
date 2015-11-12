3f50d411bf9bcb4e5ade965491933bc5a0c2a245
tprost <tprost@users.noreply.github.com>
2015-11-12 09:17:10 -0500

    put JSON mode back in

--- init/init-json-mode.el
+++ init/init-json-mode.el
@@ -1,8 +1,10 @@
 ;; (require-package 'json-mode)
 
-;; (add-hook 'json-mode-hook
-;;           (lambda ()
-;;             (make-local-variable 'js-indent-level)
-;;             (setq js-indent-level 2)))
+(require 'json-mode)
+
+(add-hook 'json-mode-hook
+          (lambda ()
+            (make-local-variable 'js-indent-level)
+            (setq js-indent-level 2)))
 
 (provide 'init-json-mode)
