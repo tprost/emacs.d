c6bc1cc8b2864fa7f968861d269923db92ecd0fb
tprost <tprost@users.noreply.github.com>
2016-01-19 11:53:50 -0500

    always use SCSS mode
    get rid of backups

--- init/init-backups.el
+++ init/init-backups.el
@@ -1,23 +1,6 @@
-;; aggh backups
-(setq auto-save-default t)
-(setq make-backup-files t)
-(setq backup-directory-alist
-      `((".*" . ,temporary-file-directory)))
-(setq auto-save-file-name-transforms
-      `((".*" ,temporary-file-directory t)))
-
-
-;; disable auto-save and auto-backup
+;; screw backing stuff up
 (setq auto-save-default nil)
-
-;; because apparently setting auto-save-default just isn't enough
-(setq auto-save-interval 0)
-
 (setq make-backup-files nil)
-                                        ;disable backup
-(setq backup-inhibited t)
-                                        ;disable auto save
-
-(setq auto-save-default nil)
+(setq create-lockfiles nil)
 
 (provide 'init-backups)
--- init/init-scss-mode.el
+++ init/init-scss-mode.el
@@ -4,7 +4,11 @@
 
 ;; (autoload 'scss-mode "scss-mode")
 
+(add-to-list 'auto-mode-alist '("\\.css\\'" . scss-mode))
 (add-to-list 'auto-mode-alist '("\\.scss\\'" . scss-mode))
+
+(set-face-attribute 'scss-variable nil
+										:foreground "orange")
 
 ;; (setq scss-compile-at-save nil)
 
