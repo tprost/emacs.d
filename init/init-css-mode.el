ab02386b8a6cb25d1c9c2effe62ee5486a0b3308
tprost <tprost@users.noreply.github.com>
2016-02-09 15:21:28 -0500

    use spaces for indents by default

--- init/init-css-mode.el
+++ init/init-css-mode.el
@@ -1,8 +1,11 @@
 ;; (add-to-list 'load-path "~/.emacs.d/site-lisp/css-mode")
 
 ;; (require 'css-mode)
-
 (setq css-indent-offset 2)
 
+(defun tp-css-mode-hook ()
+  (setq indent-tabs-mode t))
+
+(add-hook 'css-mode-hook 'tp-css-mode-hook)
 
 (provide 'init-css-mode)
--- init/init-variables.el
+++ init/init-variables.el
@@ -1,13 +1,5 @@
 (setq electric-indent-mode nil)
 
-;; because other people hate spaces I guess
-(setq-default indent-tabs-mode t)
-(setq-default tab-width 2)
-(setq css-indent-offset 2)
-
-;;    (defvaralias 'c-basic-offset 'tab-width)
-;;    (defvaralias 'cperl-indent-level 'tab-width)
-
 ;; Enable transient mark mode
 (transient-mark-mode 1)
 
