af0a69d7ed545ec53a55b59f6f1be23a7868d5db
tprost <tprost@users.noreply.github.com>
2016-02-28 16:22:39 -0500

    css related crap

--- init/init-scss-mode.el
+++ init/init-scss-mode.el
@@ -1,14 +1,14 @@
-(require-package 'scss-mode)
+;; (require-package 'scss-mode)
 
-(require 'scss-mode)
+;; (require 'scss-mode)
 
-;; (autoload 'scss-mode "scss-mode")
+;; ;; (autoload 'scss-mode "scss-mode")
 
-(add-to-list 'auto-mode-alist '("\\.css\\'" . scss-mode))
-(add-to-list 'auto-mode-alist '("\\.scss\\'" . scss-mode))
+;; (add-to-list 'auto-mode-alist '("\\.css\\'" . scss-mode))
+;; (add-to-list 'auto-mode-alist '("\\.scss\\'" . scss-mode))
 
-;; (set-face-attribute 'scss-variable nil :foreground "orange")
+;; ;; (set-face-attribute 'scss-variable nil :foreground "orange")
 
-(setq scss-compile-at-save nil)
+;; (setq scss-compile-at-save nil)
 
 (provide 'init-scss-mode)
---
+++ init/init-xcss-mode.el
@@ -0,0 +1,4 @@
+(set-face-attribute 'css-property nil :foreground "orange")
+(set-face-attribute 'css-selector nil :foreground "MediumPurple2")
+
+(provide 'init-xcss-mode)
