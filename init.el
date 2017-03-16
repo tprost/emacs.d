21df3f3408488e2a59a8e3344b060f1c70f1d9a3
tprost <tprost@users.noreply.github.com>
2017-03-16 15:08:32 -0400

    remove sonic pi and add if statement for org

--- init.el
+++ init.el
@@ -72,14 +72,3 @@
 (require 'init-cmake-mode)
 (require 'init-goldendict)
 (put 'downcase-region 'disabled nil)
-
-(add-to-list 'load-path "~/.emacs.d/site-lisp/sonic-pi")
-(require 'sonic-pi)
-(setq sonic-pi-path "~/code/sonic-pi") ; Must end with "/"
-
-;; Optionally define a hook
-(add-hook 'sonic-pi-mode-hook
-          (lambda ()
-            ;; This setq can go here instead if you wish
-            (setq sonic-pi-path "~/code/sonic-pi")
-            (define-key ruby-mode-map "\C-c\C-b" 'sonic-pi-stop-all)))
