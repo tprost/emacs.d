b0e6d920be8e7ffc4479df13f53459b234074638
Taylor Prost <taylor@Taylors-MacBook-Pro.local>
2024-08-25 14:52:10 -0400

    theme change
    - elisp format custom function
    - splash screen

--- config.el
+++ config.el
@@ -36,7 +36,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-horizon)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -332,16 +332,49 @@
 
   (evil-define-key 'evilisp 'global (kbd "x") 'paredit-backward-down)
   (evil-define-key 'evilisp 'global (kbd "<f3>") 'evil-normal-state)
-  (evil-define-key 'evilisp 'global (kbd "d") nil)
-  )
+  (evil-define-key 'evilisp 'global (kbd "d") nil))
+
 (defun +expand-region-to-parent ()
   "Expand the region to the parent form in Lisp."
   (interactive)
-  (er/expand-region 2)
-  )
+  (er/expand-region 2))
 
 ;; (use-package! bind-map)
 ;; (use-package! evil-lisp-state
 ;;   (after! evil-lisp-state
 
 ;;     (evil-lisp-state-leader "z")))
+(defun +clean-up-trailing-parentheses ()
+  "Remove any whitespace between the last symbol in a defun and the closing parenthesis."
+  (interactive)
+  (save-excursion
+    (beginning-of-defun)
+    (evil-cp-end-of-defun)                                    ; Move to the end of the defun
+    (backward-char)                     ; Move back over the last symbol
+    (while (looking-at "[ \t\n]")       ; While there's whitespace
+      (delete-char 1)
+      (backward-char))                  ; Delete the whitespace
+    (message "Trailing parentheses cleaned up!")))
+
+(defun my-dweebery-is-always-greater ()
+  (let* ((banner '(
+                   "                    Music can change the world.                  "
+                   " _______________________________________________________________ "
+                   "|| | | ||| | ||| | | ||| | ||| | | ||| | ||| | | ||| | ||| | | ||"
+                   "||_|_|_|||_|_|||_|_|_|||_|_|||_|_|_|||_|_|||_|_|_|||_|_|||_|_|_||"
+                   "| | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | | |"
+                   "|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|_|"))
+         (longest-line (apply #'max (mapcar #'length banner))))
+    (put-text-property
+     (point)
+     (dolist (line banner (point))
+       (insert (+doom-dashboard--center
+                +doom-dashboard--width
+                (concat line (make-string (max 0 (- longest-line (length line))) 32)))
+               "\n"))
+     'face 'doom-dashboard-banner)))
+
+(setq +doom-dashboard-ascii-banner-fn #'my-dweebery-is-always-greater)
+
+
+(setq default-frame-alist '((left . 0) (width . 141) (fullscreen . fullheight)))
